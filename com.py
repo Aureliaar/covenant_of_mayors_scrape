0c530745c3094ddb606a56960d4b2b2dc1f4f2b1
aureon <jacopo.sinigaglia@gmail.com>
2020-10-12 22:09:50 +0200

    code fixup

--- com.py
+++ com.py
@@ -24,12 +24,21 @@ data_types = [
 
 DATA_URL = "https://www.covenantofmayors.eu/about/covenant-community/signatories/action-plan.html?task=actionplan.{data_type}&orgid={id}"
 categories = ["overview", "baseline-review", "action-plan", "progress"]
-url = BASE_URL.format(cat=categories[0], id=13564)
 
 def dump_url(id):
     url = BASE_URL.format(cat=categories[0], id=id)
     page = requests.get(url)
     if not page: return False
+    
+    name = extract_name(page)
+    if not name: return False, False
+    
+    dumps = dump_ajax_data(id)
+    
+    
+    return name, dumps
+
+def extract_name(page):
     soup = BeautifulSoup(page.content, 'html.parser')
     data = {}
     for name, sel in selectors.items():
@@ -38,29 +47,32 @@ def dump_url(id):
         r = sanitize(r)
         data[name] = r
     name = data.get('name', False)
-    if not name: return False, False
-    
+    return name
+        
+def dump_ajax_data(id):
     dumps = {}
     for data_type in data_types:
         dumps['id'] = id
         page = requests.get(DATA_URL.format(data_type=data_type, id=id))
-        jpage = page.json()
-        if jpage.get('rstatus', False):
-            dumps[data_type] = jpage
-            names = jpage.get('rname')
-            values = jpage.get('rvalue')
-            percs = jpage.get('percentage')
-            d = {}
-            for i in range(0, len(names)):
-                d[names[i]] = values[i]
-                d[names[i]+'perc'] = percs[i]
-            dumps[data_type] = d
-        else:
-            dumps[data_type] = "N/A"
+        extract_ajax_data(page, dumps, data_type)
+    return dumps
+
+def extract_ajax_data(page, dumps, data_type):
+    jpage = page.json()
     
-    
-    return name, dumps
+    if jpage.get('rstatus', False):
+        dumps[data_type] = jpage
         
+        names = jpage.get('rname')
+        values = jpage.get('rvalue')
+        percs = jpage.get('percentage')
+        data = {}
+        for i in range(0, len(names)):
+            data[names[i]] = values[i]
+            data[names[i]+'perc'] = percs[i]
+        dumps[data_type] = data
+    else:
+        dumps[data_type] = "N/A"
         
 def sanitize(s):
     for dead in to_strip:
